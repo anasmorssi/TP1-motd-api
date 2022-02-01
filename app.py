5c7b97a67c2f34322a4b7b359763a7c7f2d65737
anasmorssi <anasmorssi@gmail.com>
2022-02-01 12:08:53 +0100

    TP1 motd-api

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return jsonify({"message": "Hello World!"})
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5555))
+    app.run(debug=True,host='0.0.0.0',port=port)
